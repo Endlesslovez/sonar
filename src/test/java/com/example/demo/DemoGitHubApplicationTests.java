33a701e5b61d592bb147fbb6a1e73c36fc74ce15
minhminh <nguyenxuanminh10@gmail.com>
2021-05-08 17:25:32 +0700

    lan1

--- src/test/java/com/example/demo/DemoGitHubApplicationTests.java
+++ src/test/java/com/example/demo/DemoGitHubApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +9,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 class DemoGitHubApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void DemoGit() {
+		String name = "minh";
+		assertEquals("minhdsd", name);
+	}
+	
+	@Test
+	 void sdsd() {
+		
 	}
 
 }
